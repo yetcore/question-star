7bbfdbcbfda68169cd320f0078b37cb61b83da10
yetcore <151349868+yetcore@users.noreply.github.com>
2024-12-18 00:54:53 +0800

    list finish

--- src/pages/manage/List.tsx
+++ src/pages/manage/List.tsx
@@ -1,11 +1,13 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, useEffect, useRef, useState } from "react";
 import styles from './Common.module.scss'
 import QuestionCard from "../../components/QuestionCard";
-import { Spin, Typography } from "antd";
+import { Empty, Spin, Typography } from "antd";
 import ListSearch from "../../components/ListSearch";
 import { getQuestionListService } from "../../services/question";
-import { useRequest } from "ahooks";
+import {  useDebounceFn, useRequest } from "ahooks";
 import useLoadQuestionListData from "../../hooks/useLoadQuestionListData";
+import { useSearchParams } from "react-router-dom";
+import { LIST_PAGE_SIZE, LIST_SEARCH_PARAM_KEY } from "../../constant";
 
 // const rawQuestionList = [
 //     {
@@ -62,7 +64,80 @@ const List: FC = () => {
     const [list,setList] = useState([])
     const [page,setPage] = useState(1)
     const [total,setTotal] = useState(0)
+    const [started,setStarted] = useState(false)//优化标记是否已经开始加载
     const haveMoreData = total > list.length
+    const [searchParams] = useSearchParams()
+    const keyword = searchParams.get(LIST_SEARCH_PARAM_KEY) || ''
+
+    //search
+    useEffect(() => {
+        setStarted(false);
+        setPage(1);
+        setList([]);
+        setTotal(0)
+    },[keyword])
+    //load
+    const { run:load,loading } = useRequest(async() => {
+        const data = await getQuestionListService({
+            page,
+            pageSize: LIST_PAGE_SIZE,
+            keyword,
+        }
+        )
+        return data
+    },{
+        manual: true,
+        onSuccess(result) {
+            const { list:l = [],total = 0 } = result
+            setList(list.concat(l))
+            setTotal(total)
+            setPage(page + 1)
+        }
+    })
+
+    //防抖
+    const containerRef = useRef<HTMLDivElement>(null)
+    const { run: tryLoadMore } = useDebounceFn(() => {
+        const elem = containerRef.current
+        if(elem == null)return 
+        const domReact = elem.getBoundingClientRect()
+        if(domReact == null)return
+        const { bottom } = domReact
+        if(bottom <= document.body.clientHeight) {
+            load()
+            setStarted(true)
+        }
+    },{
+        wait:1000
+    })
+    
+    //页面加载
+    useEffect(() => {
+        tryLoadMore()
+    },[searchParams])
+
+    //页面滚动
+    useEffect(() => {
+        if(haveMoreData){
+            window.addEventListener('scroll',tryLoadMore)
+        }
+        return () => window.removeEventListener('scroll',tryLoadMore)
+    },[searchParams,haveMoreData])
+
+    //can use useMemo funcation to catche,rely started ,loading,haveMoreData
+    const loadMoreContentElem = () => {
+        if(!started || loading) {
+            return <Spin />
+        }
+        if(total === 0) {
+           return <Empty description = '暂无数据'></Empty>
+        }
+        if(!haveMoreData) {
+            return <span>到达世界尽头</span>
+        }
+        return <span>开始加载下一页</span>
+    }
+
     return <>
     <div className={styles.header}> 
         <div className={styles.left}>
@@ -73,15 +148,17 @@ const List: FC = () => {
         </div>
     </div>
     <div className={styles.content}>
-        {loading && <div style={{ textAlign:'center'}}><Spin></Spin></div>}
-        {!loading && (list.length > 0) && 
-        list.map((q:any) => {
+        {/* { <div style={{ textAlign:'center'}}><Spin></Spin></div>} */}
+        {
+        list.length > 0 && list.map((q:any) => {
             const { _id } = q
             return <QuestionCard key={_id} {...q}></QuestionCard>
         })}
     </div>
     <div className={styles.footer}> 
-        footer
+        <div ref={containerRef}>
+           {loadMoreContentElem()}
+        </div>
     </div>
     </>
 }
